03792d27c439868cd6d3d11a530e1d7e0f2dc80b
blake <blake@watts.cc>
2025-02-26 12:51:00 -0800

    update `ALTER TABLE` formatting by unindenting statements for consistency

--- db/migrations/012_ddl_alter_party_role_table.sql
+++ db/migrations/012_ddl_alter_party_role_table.sql
@@ -1,4 +1,4 @@
 ALTER TABLE party_role
-    ALTER COLUMN valid_from SET NOT NULL,
-    ALTER COLUMN created_at SET NOT NULL,
-    ALTER COLUMN updated_at SET NOT NULL;
+ALTER COLUMN valid_from SET NOT NULL,
+ALTER COLUMN created_at SET NOT NULL,
+ALTER COLUMN updated_at SET NOT NULL;
