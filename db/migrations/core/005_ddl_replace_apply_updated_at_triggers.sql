954462fca1100e624c3afaca557651729ceb49e6
blake <blake@watts.cc>
2025-02-27 14:00:11 -0800

    fix trigger creation in `apply_updated_at_triggers` by replacing `%I` with `%s` for trigger name to avoid quoting issues with reserved table names

---
+++ db/migrations/core/005_ddl_replace_apply_updated_at_triggers.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION apply_updated_at_triggers()
+RETURNS VOID AS $$
+DECLARE r RECORD;
+BEGIN
+    FOR r IN
+        SELECT t.tablename
+          FROM pg_tables t
+          JOIN information_schema.columns c
+            ON t.tablename = c.table_name
+         WHERE c.column_name = 'updated_at'
+    LOOP
+        EXECUTE format(
+            'CREATE OR REPLACE TRIGGER trigger_update_%s
+             BEFORE UPDATE ON %I
+             FOR EACH ROW
+             WHEN (OLD.* IS DISTINCT FROM NEW.*)
+             EXECUTE FUNCTION update_timestamp();',
+            r.tablename, r.tablename
+        );
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
